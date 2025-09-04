986ced90bf155e329575ecccca533b4bdcb6ba95
BadSnail11 <BadSnail11@github.com>
2025-09-05 02:15:46 +0300

    bot codebase

--- app/bot/main.py
+++ app/bot/main.py
@@ -15,6 +15,57 @@ dp = Dispatcher()
 async def command_start_handler(message: Message) -> None:
     await message.answer("Hello! I'm a bot created with aiogram.")
 
+@dp.message(Command("add"))
+async def command_add_handler(message: Message) -> None:
+    try:
+        message_content = message.text.split()
+        if (len(message_content) != 3):
+            raise Exception()
+        _, source, interval = message_content
+        await message.answer(f"Source added: {source}")
+    except:
+        await message.answer("Wrong amount of parameters!")
+
+@dp.message(Command("list"))
+async def command_list_handler(message: Message) -> None:
+    sources_list = [{"source": "source1", "status": "200 OK"}, {"source": "source2", "status": "502 Bad Gateway"}]
+    reply_text = ""
+
+    for element in sources_list:
+        reply_text += f"{element['source']} : {element['status']}\n"
+
+    await message.answer(reply_text)    
+
+@dp.message(Command("remove"))
+async def command_remove_handler(message: Message) -> None:
+    try:
+        message_content = message.text.split()
+        if (len(message_content) != 2):
+            raise Exception()
+        _, source_id = message_content
+        await message.answer(f"Source {source_id} deleted!")
+    except:
+        await message.answer("Wrong amount of parameters!")
+
+@dp.message(Command("status"))
+async def command_status_handler(message: Message) -> None:
+    try:
+        message_content = message.text.split()
+        if (len(message_content) != 2):
+            raise Exception()
+        _, source_id = message_content
+        await message.answer(f"Source {source_id} status: 200 OK")
+    except:
+        await message.answer("Wrong amount of parameters!")
+
+@dp.message(Command("help"))
+async def command_help_handler(message: Message) -> None:
+    await message.answer("/add https://example.com 60s — добавить ресурс с интервалом проверки,\n"
+                        "/list — показать ресурсы и статус,\n"
+                        "/remove <id> — удалить,\n"
+                        "/status <id> — ручная проверка,\n"
+                        "/help — помощь")
+
 
 # Run the bot
 async def main() -> None:
